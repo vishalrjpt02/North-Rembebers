5c67fd9ecd8ecb6a89e0328dfac172a5ad86d399
Vishal Rajput <55855196+vishalrjpt02@users.noreply.github.com>
2024-01-05 19:29:49 +0530

    Add files via upload

---
+++ JuiceShopDataAcess/JuiceException.cs
@@ -0,0 +1,31 @@
+﻿/*************************************************************************************************
+ * @author  : Vishal kumar
+ * @file    : JuiceDatamanager
+ * @purpose : to add and retrive data from database i.e: ado.net and stored procedure will be used
+ * **********************************************************************************************/
+
+using System;
+using System.Runtime.Serialization;
+
+namespace JuiceShopDataAcess
+{
+    [Serializable]
+    internal class JuiceException : Exception
+    {
+        public JuiceException()
+        {
+        }
+
+        public JuiceException(string message) : base(message)
+        {
+        }
+
+        public JuiceException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected JuiceException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ JuiceShopEntity/JuiceEntity.cs
@@ -0,0 +1,19 @@
+﻿/****************************************************************************************
+ * @author  : Vishal kumar
+ * @file    : JuiceEntity.c#
+ * @purpose : to create the entity of the juice center
+ * **************************************************************************************/
+
+using System;
+namespace JuiceShopEntity
+{
+    public class JuiceEntity
+    {
+        public int JuiceId { get; set; }
+        public String Flavour { get; set; }
+        public double Price { get; set; }
+        
+        public JuiceEntity() { }
+        
+    }
+}
