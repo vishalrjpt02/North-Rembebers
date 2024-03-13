2ce6294d24af11e20332918a7405a5c6a62792c5
Vishal Rajput <vishalrjpt02@outlook.in>
2024-03-13 10:30:16 +0530

    file handeling

---
+++ text file reader/FileReading.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text_file_reader
+{
+    public  class FileReading
+    {
+        public static void Main(string[] args)
+        {
+            //StreamReader reader = new StreamReader("C:\\Users\visha\Desktop\Demotext.txt");
+            var line = "sanjay yadav : 28, bugsou jhjdhfisd : 34, ijbiuhcwniucwe : 12";
+            string path = @"C:\\Users\\visha\\Desktop\\Demotext.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    //StreamWriter writer = new StreamWriter(@"C:\Users\visha\Desktop\Demotext.txt");
+                    File.AppendAllText(@"C:\Users\visha\Desktop\Demotext.txt", line);
+                    
+                }
+                
+            }
+            else
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    File.AppendAllText(@"C:\Users\visha\Desktop\Demotext.txt", line);
+                }
+            }
+            
+            var filedata =File.ReadAllLines(path);
+            foreach(string data in filedata)
+            {
+                Console.WriteLine(line);
+            }
+           
+            
+            
+        }
+    }
+}
